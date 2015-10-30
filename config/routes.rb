829431163a31ea140033a24b487c32faf401c08c
Gustavmr <gmenende@chicagobooth.edu>
2015-10-29 20:02:40 -0500

    HW part 3

--- config/routes.rb
+++ config/routes.rb
@@ -4,36 +4,36 @@ Rails.application.routes.draw do
 
   # Routes for the Director resource:
   # CREATE
-  get('/directors/new_form', { :controller => 'directors', :action => 'new_form' })
-  get('/create_director', { :controller => 'director', :action => 'create_row' })
+  get('/directors/new_form', { :controller => 'directors', :action => 'new_form' }) #done
+  get('/create_director', { :controller => 'directors', :action => 'create_row' }) #done
 
   # READ
-  get('/directors', { :controller => 'directors', :action => 'index' })
-  get('/directors/id', { :controller => 'directors', :action => 'show' })
+  get('/directors', { :controller => 'directors', :action => 'index' }) #done
+  get('/directors/:id', { :controller => 'directors', :action => 'show' }) #done
 
   # UPDATE
-  get('/directors/:id/edit_form', { :controller => 'directors', :action => 'edit_form' })
-  get('/update_director/:id', { :controller => 'directors', :action => 'update_row' })
+  get('/directors/:id/edit_form', { :controller => 'directors', :action => 'edit_form' }) #done
+  get('/update_director/:id', { :controller => 'directors', :action => 'update_row' }) #done
 
   # DELETE
-  get('/delete_director/:id', { :controller => 'directors', :action => 'destroy' })
+  get('/delete_director/:id', { :controller => 'directors', :action => 'destroy' }) #done
   #------------------------------
 
   # Routes for the Movie resource:
   # CREATE
-  get('/movies/new_form', { :controller => 'movies', :action => 'new_form' })
-  get('/create_movie', { :controller => 'movies', :action => 'create_row' })
+  get('/movies/new_form', { :controller => 'movies', :action => 'new_form' }) #done
+  get('/create_movie', { :controller => 'movies', :action => 'create_row' }) #done
 
   # READ
-  get('/movies', { :controller => 'movies', :action => 'index' })
-  get('/movies/:id', { :controller => 'movies', :action => 'show' })
+  get('/movies', { :controller => 'movies', :action => 'index' }) #done
+  get('/movies/:id', { :controller => 'movies', :action => 'show' }) #done
 
   # UPDATE
-  get('/movies/:id/edit_form', { :controller => 'movies', :action => 'edit_form' })
-  get('/update_movie/:id', { :controller => 'movies', :action => 'update_row' })
+  get('/movies/:id/edit_form', { :controller => 'movies', :action => 'edit_form' }) #done
+  get('/update_movie/:id', { :controller => 'movies', :action => 'update_row' }) #done
 
   # DELETE
-  get('/delete_movie/:id', { :controller => 'movies', :action => 'destroy' })
+  get('/delete_movie/:id', { :controller => 'movies', :action => 'destroy' }) #done
   #------------------------------
 
   # Routes for the Role resource:
